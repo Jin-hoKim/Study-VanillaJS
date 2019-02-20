cbb1228e6975864b9b0a39f97c5e8f5c36c7eaa3
Jin-hoKim <wltn222@gmail.com>
2019-02-20 15:27:05 +0900

    study js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-console.log("it working. it's JS");
+
+const arrDays = ["Mon", "Tue", "Web", "Thu", "Fri", "Sat"];
+
+for (let day of arrDays)
+{
+  console.log(day);
+}
+
+const obj = {
+  name: "JH",
+  age: 37,
+  gender: "Male"
+};
+
+for( let prop in obj )
+{
+  console.log(prop, obj[prop]);
+}
+
+// js - function
+function sayHello( name )
+{
+  console.log( "Gretting, ", name, "!" );
+  console.log( `Hello, ${name}` );
+}
+
+sayHello("JHK");
+
+
+
+// console.log( arrDays );
